907301325dd6eb2bd47a241e58a67ed967e53aa7
Fernando Monnot <fermonnot@gmail.com>
2022-09-05 20:52:45 -0400

    firs Commit

--- src/main.py
+++ src/main.py
@@ -8,12 +8,14 @@ from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User
+from models import db, Character, Planets, Favorites, User
 #from models import Person
 
 app = Flask(__name__)
 app.url_map.strict_slashes = False
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DB_CONNECTION_STRING')
+db_url = os.getenv('DATABASE_URL')
+# app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DB_CONNECTION_STRING')
+app.config['SQLALCHEMY_DATABASE_URI'] = db_url.replace("postgres://", "postgresql://")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 MIGRATE = Migrate(app, db)
 db.init_app(app)
@@ -30,16 +32,156 @@ def handle_invalid_usage(error):
 def sitemap():
     return generate_sitemap(app)
 
-@app.route('/user', methods=['GET'])
-def handle_hello():
 
-    response_body = {
-        "msg": "Hello, this is your GET /user response "
-    }
 
-    return jsonify(response_body), 200
+
+
+
+ 
+
+@app.route('/people', methods=['POST'])
+def add_new_character():
+    if request.method == 'POST':
+        body = request.json
+        if body.get("name") is None:
+            return {"message":"error.propertie bad"}, 400
+        
+        if body.get("height") is None:
+            return {"message":"error.propertie bad"}, 400
+        
+        new_character = Character(name=body["name"],height=body.get("height"))
+        db.session.add(new_character)
+
+        try:
+            db.session.commit()
+            return jsonify(new_character.serialize()), 201
+        except Exception as error:
+            print(error.args)
+            db.session.rollback()
+            return jsonify ({"message":f"Error {error.args}"}), 500
+
+
+@app.route('/planets', methods=['GET']) 
+@app.route('/planets/<int:planet_id>', methods=['GET']) 
+def handle_planet(planet_id = None):
+    if request.method == 'GET':
+        if planet_id is None:
+            planets = Planets()
+            planets = planets.query.all()
+            return jsonify(list (map(lambda item: item.serialize(), planets))), 200  
+        else:
+            planet = Planet()
+            planet = query.get(planet_id)
+            if planet:
+                return jsonify (planet.serialize())
+
+        return jsonify ({"message":"Not Found"}), 404
+
+
+
+@app.route('/planets', methods=['POST'])
+def add_new_planet():
+    if request.method == 'POST':
+        body = request.json
+        if body.get("name") is None:
+            return {"message":"error.propertie bad"}, 400
+        
+        if body.get("population") is None:
+            return {"message":"error.propertie bad"}, 400
+        
+        new_planet = Planets(name=body["name"],population=body.get("population"))
+        db.session.add(new_planet)
+
+        try:
+            db.session.commit()
+            return jsonify(new_planet.serialize()), 201
+        except Exception as error:
+            print(error.args)
+            db.session.rollback()
+            return jsonify ({"message":f"Error {error.args}"}), 500
+
+
+@app.route('/favorites/', methods=['GET'])
+@app.route('/favorites/<int:user_id>', methods=['GET'])     
+def handle_Favorites(user_id = None):
+    if request.method == 'GET':
+        if user_id is not None:
+           
+            favorites = Favorites.query.filter_by(id_user=user_id).all()
+            print(favorites)
+            return jsonify(favorites), 200 
+        # else:
+        #     favorite = Favorite()
+        #     favorite = query.get(user_id)
+        #     if planet:
+        #         return jsonify (planet.serialize())
+
+        return jsonify ({"message":"Not Found"}), 404
+    
+    else:
+
+        return jsonify ({"message":f"method not accepted"}), 405
+
+
+
+
+
+@app.route('/favorites/<int:user_id>/<nature>', methods=['POST'])
+def add_favoites(user_id = None, nature = None):
+    if request.method == 'POST':
+        if user_id is not None and nature is not None:
+            print(user_id, nature)
+            user = User.query.filter_by(id=user_id).first()
+            if user: 
+                body = request.json
+                if nature is not None and body['name'] is not None:
+                    new_favorite = Favorites(id_user = user_id, nature=nature, name=body['name'])
+                    db.session.add(new_favorite)
+                    try:
+                        db.session.commit()
+                        return jsonify(new_favorite.serialize()), 201
+                    except Exception as error:
+                        db.session.rollback()
+                        return jsonify({"message":error.args}), 500
+                print(user.serialize())
+                return "hola" 
+            else: 
+                return jsonify ({"message":f"user not Found"}), 404
+    else:
+        return jsonify ({"message":f"method not accepted"}), 405
+
+
+@app.route('/favorites', methods=['DELETE'])
+@app.route('/favorites/<int:user_id>/<int:favorite_id>', methods=['DELETE'])
+def delete_favorite(user_id=None):
+    if request.method == 'DELETE':
+        if user_id is None:
+            return jsonify({"message":"Not found"}), 400
+
+        if user_id is not None:
+            delete_favorite = Human.query.get(user_id)
+            
+            if delete_human is None:
+                return jsonify({"message":"Not found"}), 404
+            else:
+                db.session.delete(delete_human)
+
+                try:
+                    db.session.commit()
+                    return jsonify([]), 204
+                except Exception as error:
+                    print(error.args)
+                    db.session.rollback()
+                    return jsonify({"message":f"Error {error.args}"}),500
+        
+    return jsonify([]), 405
+
+
+
+
 
 # this only runs if `$ python src/main.py` is executed
 if __name__ == '__main__':
     PORT = int(os.environ.get('PORT', 3000))
     app.run(host='0.0.0.0', port=PORT, debug=False)
+
--- src/models.py
+++ src/models.py
@@ -1,15 +1,56 @@
 from flask_sqlalchemy import SQLAlchemy
+from enum import Enum, unique
 
 db = SQLAlchemy()
+
+class Nature(Enum):
+    character = "character"
+    planets = "planets"
+
+
+class Character (db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), unique=True, nullable=False)
+    height = db.Column(db.String(80), unique=False, nullable=False)
+
+    @classmethod 
+    def create(self):
+        pass
+    
+    def serialize(self):
+        return {
+            "id" : self.id,
+            "name": self.name,
+            "heigth": self.height
+        }
+
+
+
+class Planets (db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), unique=True, nullable=False)
+    population = db.Column(db.String(80), unique=False, nullable=False)
+
+    @classmethod 
+    def create(self):
+        pass
+    
+    def serialize(self):
+        return {
+            "id" : self.id,
+            "name": self.name,
+            "population": self.population
+        }
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
-
+    favorites = db.relationship("Favorites", backref="user", uselist=True)
+    
     def __repr__(self):
-        return '<User %r>' % self.username
+        return '<User %r>' % self.email
 
     def serialize(self):
         return {
@@ -17,3 +58,29 @@ class User(db.Model):
             "email": self.email,
             # do not serialize the password, its a security breach
         }
+
+
+
+class Favorites(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    id_user = db.Column(db.Integer, db.ForeignKey('user.id'))
+    nature = db.Column(db.Enum(Nature), nullable=False)
+    name = db.Column(db.String(50), nullable=False)    
+
+    __table_args__ = (db.UniqueConstraint(
+        "id_user",
+        "name",
+        name="unique_favorites_for_user"
+        ),)
+
+    def serialize(self):
+        return {
+            "id" : self.id,
+            "name": self.name,
+            "nature": self.nature.name,
+            "id_user": self.id_user
+        }
+    
+    
+
+
